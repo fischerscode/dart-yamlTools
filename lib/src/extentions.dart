24a891055ab5f471287be2c1879c9e954ef12edc
Maximilian Fischer <github@maaeps.de>
2021-09-29 12:23:41 +0200

    extended functionality
    - changed `ExtendedYamlNode`:
      - added `asBool()`
      - added `asNum()`
      - added `asInt()`
        - added `asDouble()`
    - changed `ExtendedYamlMap`:
      - fixed `bool` and `num` values
    - added `ExtendedYamlList`

--- lib/src/extentions.dart
+++ lib/src/extentions.dart
@@ -20,6 +20,32 @@ extension ExtendedYamlNode on YamlNode {
     return asScalar()?.toString();
   }
 
+  bool? asBool() {
+    var value = asScalar()?.value;
+    if (value is bool) {
+      return value;
+    } else {
+      return null;
+    }
+  }
+
+  num? asNum() {
+    var value = asScalar()?.value;
+    if (value is num) {
+      return value;
+    } else {
+      return null;
+    }
+  }
+
+  int? asInt() {
+    return asNum()?.toInt();
+  }
+
+  double? asDouble() {
+    return asNum()?.toDouble();
+  }
+
   YamlNode? getMapValue(String key) {
     return asMap()?.get(key);
   }
@@ -29,6 +55,18 @@ extension ExtendedYamlMap on YamlMap {
   YamlNode? get(String key) {
     if (this[key] is YamlNode) return this[key] as YamlNode;
     if (this[key] is String) return YamlScalar.wrap(this[key]);
+    if (this[key] is bool) return YamlScalar.wrap(this[key]);
+    if (this[key] is num) return YamlScalar.wrap(this[key]);
+    return null;
+  }
+}
+
+extension ExtendedYamlList on YamlList {
+  YamlNode? get(int index) {
+    if (this[index] is YamlNode) return this[index] as YamlNode;
+    if (this[index] is String) return YamlScalar.wrap(this[index]);
+    if (this[index] is bool) return YamlScalar.wrap(this[index]);
+    if (this[index] is num) return YamlScalar.wrap(this[index]);
     return null;
   }
 }
